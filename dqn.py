caefe1fdb68cf5b93e434513a77b79316010f514
Michael Djorup <michael.djorup@gmail.com>
2022-03-21 15:14:59 -0400

    Implemented StateProcessor class

--- dqn.py
+++ dqn.py
@@ -6,13 +6,49 @@ from preprocessing import phi
 import replay_memory
 
 
+#gamma
+gamma = 0.99 #discount factor
 
+
+#frame count
+frame = 0
+#max frames to train on
+max_frames = 1000000
+#how many frames to be random actions to build initial memory
+random_frames = 10000 
+#how many frames to decay epsilon 
+e_greedy_frames = 200000
+#how often to update model, memory added on every frame
+update_interval = 4
+
+#Frequency in which random actions are taken
+epsilon = 1
+#lowest possible frequency for random actions
+min_epsilon = 0.1
+#epsilon decrement on each frame
+epsilon_decrement = (epsilon - min_epsilon) / e_greedy_frames
+
+
+
+batch_sample_size = 32
 
 
 memory = replay_memory.ReplayMemory(10000)
 
 
-print(memory.curr_capacity)
+print(memory.max_capacity)
 # macros
+
+
+
+#logic 
+# We have initial image x
+# We need to turn image x into s0 in initialization
+# We get new image
+    # Need to process image
+    # Add it to s
+    #remove first x in s
+    #now we have s1
+
 
 
---
+++ state.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+
+def process_single_frame(frame):
+    # x should be a single numpy array of shape (210, 160, 3)
+    grayscale = tf.image.rgb_to_grayscale(frame)
+    resize = tf.image.resize(grayscale, [110, 84])
+    cropped = tf.image.crop_to_bounding_box(resize, 17, 0, 84, 84)
+    return cropped.numpy() # output shape (84, 84, 1)
+
+
+class StateProcessor:
+
+    def __init__(self, initial_frame, length=4):
+        x = process_single_frame(initial_frame)
+        self.state = np.repeat([x], 4, axis=0)
+
+
+    def insert_frame(self, frame):
+        x = process_single_frame(frame)
+        appended = np.append(self.state, [x], axis=0)
+        self.state = np.remove(appended, 0, 0)
+
+    
